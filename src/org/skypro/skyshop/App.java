6955c0ee3eaa6f53686729fd489581a26d8a706e
andyxsly <andyxsly@bk.ru>
2025-11-20 01:48:57 +0300

    Complete

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -1,42 +1,36 @@
 package org.skypro.skyshop;
 
+import org.skypro.skyshop.basket.ProductBasket;
+import org.skypro.skyshop.product.*;
+
 public class App {
     public static void main(String[] args) {
 
-        Product apple = new Product("Яблоко", 65);
-        Product bread = new Product("Хлеб", 45);
-        Product juice = new Product("Сок", 137);
-        Product milk = new Product("Молоко", 120);
-        Product orange = new Product("Апельсин", 110);
-        Product cheese = new Product("Сыр", 680);
-        Product coffee = new Product("Кофе", 370);
-        Product chocolate = new Product("Шоколад", 90);
+        Product apple = new SimpleProduct("Яблоко", 50);
+        Product cheese = new DiscountedProduct("Сыр", 200, 25);
+        Product coffee = new FixPriceProduct("Кофе");
+        Product book = new DiscountedProduct("Книга", 500, 10);
+        Product tea = new SimpleProduct("Чай", 80);
 
         ProductBasket basket = new ProductBasket();
 
         basket.addProduct(apple);
-        basket.addProduct(bread);
-        basket.addProduct(juice);
-        basket.addProduct(milk);
-        basket.addProduct(orange);
         basket.addProduct(cheese);
         basket.addProduct(coffee);
-        basket.addProduct(chocolate);
+        basket.addProduct(book);
+        basket.addProduct(tea);
+
+        basket.addProduct(new SimpleProduct("Груша", 60));
 
         basket.printBasket();
 
-        System.out.println("Общая стоимость: " + basket.getTotalPrice());
+        System.out.println("Стоимость корзины: " + basket.getTotalPrice());
 
-        System.out.println("Есть ли Апельсин? " + basket.containsProduct("Апельсин"));
-
-        System.out.println("Есть ли Хлеб? " + basket.containsProduct("Хлеб"));
+        System.out.println("Есть ли Кофе? " + basket.containsProduct("Кофе"));
+        System.out.println("Есть ли Молоко? " + basket.containsProduct("Молоко"));
 
         basket.clear();
 
         basket.printBasket();
-
-        System.out.println("Общая стоимость после очищения корзины: " + basket.getTotalPrice());
-
-        System.out.println("Есть ли Шоколад после очищения корзины? " + basket.containsProduct("Шоколад"));
     }
 }
